b870de39695e35994775892ba92a9329f043ec64
harshit-8723 <harshitbhatt8723@gmail.com>
2025-05-11 22:06:17 +0530

    setup :: frontend, configure the frontend for tailwindcss and chakar UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='text-5xl bg-blue-800 underline flex justify-center p-8'>Hello World!!</h1>
+    <Box p={6} bg="gray.50" minH="100vh">
+      <Heading mb={4}>Hello from Chakra UI</Heading>
+      <Text fontSize="lg" mb={4}>
+        Chakra UI is working correctly!
+      </Text>
+      <Button colorScheme="teal" size="lg">
+        Click Me
+      </Button>
+    </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
