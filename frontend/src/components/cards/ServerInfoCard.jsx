7e85a86b8918f32628cd6338f589c1cd17f4115c
harshit-8723 <harshitbhatt8723@gmail.com>
2025-06-14 22:00:32 +0530

    modify and fix color error

--- frontend/src/components/cards/ServerInfoCard.jsx
+++ frontend/src/components/cards/ServerInfoCard.jsx
@@ -1,8 +1,20 @@
-import { Card, CardHeader, Heading, CardBody, Text, Box } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  Text,
+  Box,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import KeyValueList from "../KeyValueList";
 
 const ServerInfoCard = ({ siteData }) => {
   if (!siteData?.server_info) return null;
+
+  const boxBg = useColorModeValue("gray.50", "gray.90");
+  const textColor = useColorModeValue("black", "white");
+  const borderColor = useColorModeValue("gray.300", "gray.600");
 
   return (
     <Card>
@@ -10,10 +22,19 @@ const ServerInfoCard = ({ siteData }) => {
         <Heading size="md">Server Info</Heading>
       </CardHeader>
       <CardBody>
-        <Text>
+        <Text mb={2}>
           <b>Status Code:</b> {siteData.server_info.status_code || "N/A"}
         </Text>
-        <Box maxH="150px" overflowY="auto" mt={2} fontSize="sm" bg="gray.50" p={2} borderRadius="md">
+        <Box
+          maxH="300px"
+          overflowY="auto"
+          bg={boxBg}
+          color={textColor}
+          p={4} 
+          border="1px solid"
+          borderColor={borderColor}
+          borderRadius="md"
+        >
           <KeyValueList data={siteData.server_info.headers || {}} />
         </Box>
       </CardBody>
